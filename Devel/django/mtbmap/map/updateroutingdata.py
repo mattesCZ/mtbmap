a039c5da50cbe4cf7b1561f6b0b18fc4055b1b00
mattesCZ <osmmtb@gmail.com>
2013-03-21 20:40:31 +0100

    Update of routing data better implemented  - faster and with highway areas and osmc values

--- Devel/django/mtbmap/map/updateroutingdata.py
+++ Devel/django/mtbmap/map/updateroutingdata.py
@@ -1,39 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#from django.contrib.gis.geos.geometry import GEOSGeometry
 from django.db import connection, transaction
-from map.models import Way#, Vertice
+from map.models import Way
 import psycopg2
 import simplejson as json
+from datetime import datetime
 
 sac_scale_values = ['hiking', 'mountain_hiking', 'demanding_mountain_hiking',
                     'alpine_hiking', 'demanding_alpine_hiking', 'difficult_alpine_hiking']
 
-def dictfetchall(cursor):
-    "Returns all rows from a cursor as a dict"
-    desc = cursor.description
-    return [
-        dict(zip([col[0] for col in desc], row))
-        for row in cursor.fetchall()
-    ]
-
-#def string_to_type(value, type):
-#    try:
-#        if type=='string':
-#            return value
-#        if type=='int':
-#            res = int(value)
-#        elif type=='float':
-#            res = float(value)
-#        else:
-#            return None
-#    except ValueError:
-#        return None
-#    else:
-#        return res
-#
 def copy_ways():
+    '''
+    copy data generated with osm2po to map_way table
+    '''
     cursor = connection.cursor()
     cursor.execute('DELETE FROM map_way')
     insert = """
@@ -45,71 +25,122 @@ def copy_ways():
     transaction.commit_unless_managed()
     print "All ways uploaded successfully"
 
-#def create_vertices():
-#    Vertice.objects.all().delete()
-#    vertices = {}
-#    for w in Way.objects.all():
-#        vertices[w.source] = GEOSGeometry('POINT(%s %s)' % (w.x1, w.y1))
-#        vertices[w.target] = GEOSGeometry('POINT(%s %s)' % (w.x2, w.y2))
-#    for id, geom in vertices.items():
-#        vertice = Vertice(id=id, the_geom=geom)
-#        vertice.save()
-#
-#def copy_vertices():
-#    cursor = connection.cursor()
-#    cursor.execute('DELETE FROM map_vertice')
-#    insert = """
-#       insert into map_vertice (id, the_geom)
-#       select id, the_geom
-#       from vertices_tmp
-#    """
-#    cursor.execute(insert)
-#    transaction.commit_unless_managed()
-#    print "All vertices uploaded successfully"
-#
-#def copy_nodes():
-#    cursor = connection.cursor()
-#    cursor.execute('DELETE FROM map_node')
-#    insert = """
-#       insert into map_node (id, the_geom)
-#       select id, ST_SetSRID(ST_Point(lon, lat), 4326) as the_geom
-#       from nodes
-#    """
-#    cursor.execute(insert)
-#    transaction.commit_unless_managed()
-#    print "All nodes uploaded successfully"
-
 def add_attributes():
+    '''
+    Add attributes from osm2pgsql created database to our Way objects
+    '''
+    _add_line_attributes()
+    _add_polygon_attributes()
+    _add_routes_attributes()
+
+def _row_to_arguments(row):
+    '''
+    Get database row as column:value pairs dictionary, create kwargs suitable for Way updates
+    return
+    '''
+    update_args = {}
+    for key, value in row.items():
+        if value:
+            if key in ('tracktype', 'width', 'mtbscale', 'mtbscaleuphill'):
+                floatvalue = _to_float(value)
+                if key == 'width':
+                    update_args[key] = floatvalue
+                else:
+                    if floatvalue:
+                        update_args[key] = int(floatvalue)
+                    else:
+                        update_args[key] = None
+            elif key in ('sac_scale'):
+                if value in sac_scale_values:
+                    update_args[key] = sac_scale_values.index(value)
+            else:
+                update_args[key] = value
+    return update_args
+
+def _add_line_attributes():
+    '''
+    Copy useful attributes from planet_osm_line
+    '''
+    start = datetime.now()
     connection = psycopg2.connect("dbname='gisczech' user='xtesar7' password='' port='5432'")
     cursor = connection.cursor()
-    ways = Way.objects.all()
-    for way in ways:
-        select_lines = 'SELECT tracktype, oneway, access, bicycle, foot, incline, width, surface, smoothness, maxspeed, "mtb:scale" as mtbscale, "mtb:scale:uphill" as mtbscaleuphill, sac_scale, network, highway FROM planet_osm_line WHERE osm_id=%i' % (way.osm_id)
-        cursor.execute(select_lines)
-        rows = dictfetchall(cursor)
-        for row in rows:
-            for key, value in row.items():
-#                way.__dict__[key] = value
-                if value:
-                    if key in ('tracktype', 'width', 'mtbscale', 'mtbscaleuphill'):
-                        floatvalue = to_float(value)
-                        if key == 'width':
-                            way.__dict__[key] = floatvalue
-                        else:
-                            if floatvalue:
-                                way.__dict__[key] = int(floatvalue)
-                            else:
-                                way.__dict__[key] = None
-                    elif key in ('sac_scale'):
-                        if value in sac_scale_values:
-                            way.__dict__[key] = sac_scale_values.index(value)
-                    else:
-                        way.__dict__[key] = value
-        way.save()
+    query = '''SELECT osm_id, tracktype, oneway, access, bicycle, foot, incline, width, surface, smoothness, maxspeed, "mtb:scale" as mtbscale, "mtb:scale:uphill" as mtbscaleuphill, sac_scale, network, highway FROM planet_osm_line WHERE osm_id>0; '''
+    cursor.execute(query)
+    description = cursor.description
+    evaluated = 0
+    updated = 0
+    while True:
+        lines_rows = [dict(zip([col[0] for col in description], row)) for row in cursor.fetchmany(100)]
+        if not lines_rows:
+            break
+        for row in lines_rows:
+            osm_id = row['osm_id']
+            update_args = _row_to_arguments(row)
+            updated += Way.objects.filter(osm_id=osm_id).update(**update_args)
+        evaluated += 100
+        if not evaluated % 1000:
+            print evaluated, 'evaluated ways...'
     cursor.close()
-    print 'All attributes updated successfully'
+    print updated, 'ways updated successfully,', evaluated, 'lines evaluated.'
+    print 'Time:', (datetime.now()-start).total_seconds()
 
-def to_float(value):
+def _add_polygon_attributes():
+    '''
+    Copy useful attributes from planet_osm_polygon for routable areas
+    '''
+    start = datetime.now()
+    connection = psycopg2.connect("dbname='gisczech' user='xtesar7' password='' port='5432'")
+    cursor = connection.cursor()
+    evaluated = 0
+    updated = 0
+    # highway areas are still without attributes
+    null_ways_osm_ids = Way.objects.filter(highway__isnull=True).distinct('osm_id').values_list('osm_id', flat=True)
+    print len(null_ways_osm_ids), 'IDs to be evaluated...'
+    for osm_id in null_ways_osm_ids:
+        query = '''SELECT osm_id, tracktype, oneway, access, bicycle, foot, incline, width, surface, smoothness, maxspeed, "mtb:scale" as mtbscale, "mtb:scale:uphill" as mtbscaleuphill, sac_scale, network, highway FROM planet_osm_polygon WHERE osm_id=%s; ''' % osm_id
+        cursor.execute(query)
+        description = cursor.description
+        rows = [dict(zip([col[0] for col in description], row)) for row in cursor.fetchall()]
+        for row in rows:
+            update_args = _row_to_arguments(row)
+            updated += Way.objects.filter(osm_id=osm_id).update(**update_args)
+        evaluated += 1
+        if not evaluated % 1000:
+            print evaluated, 'evaluated ways...'
+    cursor.close()
+    print updated, 'ways updated successfully'
+    print 'Time:', (datetime.now()-start).total_seconds()
+
+def _add_routes_attributes():
+    '''
+    Copy useful attributes from planet_osm_routes2, this time only osmc
+    '''
+    start = datetime.now()
+    connection = psycopg2.connect("dbname='gisczech' user='xtesar7' password='' port='5432'")
+    cursor = connection.cursor()
+    routes_query = '''SELECT osm_id FROM planet_osm_routes2 WHERE osmcsymbol0<>'mtb:white:mtb_mtb' and osmcsymbol0 is not null;'''
+    cursor.execute(routes_query)
+    evaluated = 0
+    updated = 0
+    while True:
+        routes_rows = cursor.fetchmany(100)
+        if not routes_rows:
+            break
+        for row in routes_rows:
+            osm_id = row[0]
+            updated += Way.objects.filter(osm_id=osm_id).update(osmc='use')
+        evaluated += 100
+        if not evaluated % 1000:
+            print evaluated, 'evaluated records:'
+    print updated, 'osmc attrs updated successfully'
+    print 'Time:', (datetime.now()-start).total_seconds()
+
+
+def _to_float(value):
+    '''
+    String to float for width and mtbscale parsing.
+    '''
+    # Replace expected characters to floatable strings
     cleansed = value.replace(',', '.').replace('+', '.3').replace('-','.7').replace('grade', '')
     try:
         r = float(cleansed)
