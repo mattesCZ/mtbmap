23619b201f9a5a0b438a45634f2d1c94c9bb2400
mattesCZ <osmmtb@gmail.com>
2013-08-30 13:04:08 +0200

    Update scripts rewritten as management commands

---
+++ osm_data_processing/management/commands/update_routing_data.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Django imports
+from django.core.management.base import BaseCommand
+
+# Local imports
+from osm_data_processing.updateroutingdata import copy_ways, add_attributes
+
+class Command(BaseCommand):
+    args = '<configuration file path>'
+    help = 'Update data used for routing.'
+
+    def handle(self, *args, **options):
+        copy_ways()
+        add_attributes()
+        self.stdout.write("Successfully updated routing data.")
