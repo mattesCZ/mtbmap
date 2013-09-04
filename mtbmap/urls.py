4f79a9d01e4186c6c09869cd880042a044c2f291
mattesCZ <osmmtb@gmail.com>
2013-09-04 16:14:12 +0200

    Updated urls to future support i18n_patterns

--- mtbmap/urls.py
+++ mtbmap/urls.py
@@ -1,14 +1,12 @@
-from django.conf.urls import *
+from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
-
-from django.conf.urls.static import static
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
-    url(r'^$', 'map.views.index'),
+    # Add urls from the map application
+    url('^', include('map.urls')),
 
     # Password reset feature
     url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
@@ -23,12 +21,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-
 urlpatterns += patterns('',
-    url(r'^map/', include('map.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-## polls tutorial app
-#urlpatterns += patterns('',
-#    url(r'^polls/', include('polls.urls')),
-#)
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+)
