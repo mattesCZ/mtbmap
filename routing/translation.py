cdbc972b0607680feda65a39a95ffae169d2d5cd
mattesCZ <osmmtb@gmail.com>
2013-09-12 11:30:38 +0200

    Functions to dump and load routing weights translations

--- routing/translation.py
+++ routing/translation.py
@@ -2,6 +2,7 @@
 
 # Global imports
 import csv
+import os.path
 
 # Django imports
 from django.conf import settings
@@ -9,25 +10,55 @@ from django.conf import settings
 # Local imports
 from routing.models import *
 
-def copy_translatable_field(target_model, field_name, source_model):
-    for lang_code, lang_name in settings.LANGUAGES:
-        local_field_name = '_'.join([field_name, lang_code])
-        setattr(target_model, local_field_name, getattr(source_model, local_field_name))
+LANG_CODES = [lang_code for lang_code, lang_name in settings.LANGUAGES]
+MODEL_NAMES = ['WeightClass', 'Weight', 'Preferred']
 
-def sync_with_empty_weight_collection(weight_collection):
-    empty_col = WeightCollection.objects.get(slug='empty')
-    empty_col_classes = empty_col.weightclass_set.all()
-    for c in weight_collection.weightclass_set.all():
-        empty_col_c = empty_col_classes.get(slug=c.slug)
-        copy_translatable_field(c, 'name', empty_col_c)
-        c.link = empty_col_c.link
-        c.save()
-        for w in c.weight_set.all():
-            empty_col_w = empty_col_c.weight_set.get(slug=w.slug)
-            copy_translatable_field(w, 'name', empty_col_w)
-            w.save()
-    empty_col_preferreds = empty_col.preferred_set.all()
-    for p in weight_collection.preferred_set.all():
-        empty_col_p = empty_col_preferreds.get(slug=p.slug)
-        copy_translatable_field(p, 'name', empty_col_p)
-        p.save()
+def dump_translation_files(lang_code):
+    name_field = 'name_%s' % lang_code
+    directory = _get_locale_directory(lang_code)
+    for model_name in MODEL_NAMES:
+        filename = os.path.join(directory, '%s.csv' % model_name.lower())
+        fields = ['slug', 'name_en', name_field]
+        if model_name=='Weight':
+            fields = ['slug', 'weight_class__slug', 'name_en', name_field]
+        _write_csv(filename, model_name, fields)
+
+def load_translation_files(lang_code):
+    name_field = 'name_%s' % lang_code
+    directory = _get_locale_directory(lang_code)
+    for model_name in MODEL_NAMES:
+        filename = os.path.join(directory, '%s.csv' % model_name.lower())
+        model = globals()[model_name]
+        with open(filename, 'r') as f:
+            reader = csv.DictReader(f)
+            line = 2
+            for row_dict in reader:
+                new_value = row_dict[name_field]
+                if new_value:
+                    try:
+                        kwargs = {'slug':row_dict['slug']}
+                        if model_name=='Weight':
+                            kwargs['weight_class__slug'] = row_dict['weight_class__slug']
+                        obj = model.objects.get(**kwargs)
+                        if getattr(obj, name_field) != new_value:
+                            setattr(obj, name_field, new_value)
+                            obj.save()
+                            print 'Updating slug=%s, %s = %s' % (row_dict['slug'], name_field, new_value)
+                    except model.DoesNotExist:
+                        print 'Slug %s on line %i not found in the db!' % (row_dict['slug'], line)
+                line += 1
+
+def _get_locale_directory(lang_code):
+    return 'routing/locale/%s/' % lang_code
+
+def _write_csv(filename, model_name, fields):
+    with open(filename, 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
+        model = globals()[model_name]
+        if 'slug' in fields:
+            for row in model.objects.order_by('slug').values_list(*fields):
+                writer.writerow(row)
+        else:
+            for row in model.objects.values_list(*fields):
+                writer.writerow(row)
