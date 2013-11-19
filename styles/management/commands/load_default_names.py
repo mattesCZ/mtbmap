42d7e7245e84756ea716ce04d7e86d1150998049
mattesCZ <osmmtb@gmail.com>
2013-11-19 17:36:44 +0100

    Added management command to (re)load defalult map object names and translations.

---
+++ styles/management/commands/load_default_names.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Django imports
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+# Local imports
+from styles.translation import load_default_names, load_translation_file
+
+class Command(BaseCommand):
+    help = 'Load or update names of map objects used in legend.'
+
+    def handle(self, *args, **options):
+        load_default_names()
+        for lang_code, lang_name in settings.LANGUAGES:
+            if lang_code != settings.LANGUAGE_CODE:
+                load_translation_file(lang_code)
