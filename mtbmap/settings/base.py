6e59e18b524b327f235aebb2723728ce26646d64
mattesCZ <osmmtb@gmail.com>
2015-04-26 08:04:13 +0200

    #95 Initialized console logging config.

--- mtbmap/settings/base.py
+++ mtbmap/settings/base.py
@@ -132,6 +132,45 @@ LANGUAGES = (
     ('cs', u'Česky'),
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'map': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'osm_data_processing': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'routing': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'styles': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 MAPNIK_STYLES = root('styles', 'mapnik', 'my_styles/')
 SRTM_DATA = root('Data', 'shadingdata', 'SRTMv2/')
 
